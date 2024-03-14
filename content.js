2d56cef1efdf0e9fc9de242714a5b42591e698ec
Roland Yang <ryang25@governors.school>
2024-03-13 21:47:01 -0400

    fix

--- content.js
+++ content.js
@@ -1,15 +1,15 @@
 async function sendLinks() {
-	const incomplete = document.querySelectorAll(".started, .notstarted");
-	window.alert(incomplete);
-	const links = [...incomplete].filter(n => n.firstElementChild).map(n => n.firstElementChild.href);
-	await chrome.runtime.sendMessage(links);
+    const incomplete = document.querySelectorAll(".started, .notstarted");
+    window.alert(incomplete);
+    const links = [...incomplete].filter(n => n.firstElementChild).map(n => n.firstElementChild.href);
+    await chrome.runtime.sendMessage(links);
 }
 
 chrome.runtime.onMessage.addListener(() => {
-	const button = document.getElementById("completionButton");
-	if (button)
-		return button.click();
-	return true;
+    const button = document.getElementById("completionButton");
+    if (button)
+        return button.click();
+    return true;
 });
 
 document.getElementById("assignment_name").onclick = sendLinks;
--- runestone.js
+++ runestone.js
@@ -1,36 +1,36 @@
 // open as little tabs as possible.
 
 async function openAndMark(urls) {
-	chrome.runtime.onMessage.removeListener(openAndMark);
-	const props = urls.map(url => { return { active: false, url } });
-	let tab = await chrome.tabs.create({ active: false });
-	for (const prop of props) {
-		console.log(prop);
-		await chrome.tabs.update(tab.id, prop, async wait => {
-			const block = new Promise(resolve => {
-				const listener = request => {
-					chrome.runtime.onMessage.removeListener(listener);
-					if (request === false)
-						tab = chrome.tabs.create({ active: false });
-					resolve(request);
-				}
-				chrome.runtime.onMessage.addListener(listener);
-			});
-			await block;
-		});
+    chrome.runtime.onMessage.removeListener(openAndMark);
+    const props = urls.map(url => { return { active: false, url } });
+    let tab = await chrome.tabs.create({ active: false });
+    for (const prop of props) {
+        console.log(prop);
+        await chrome.tabs.update(tab.id, prop, async wait => {
+            const block = new Promise(resolve => {
+                const listener = request => {
+                    chrome.runtime.onMessage.removeListener(listener);
+                    if (request === false)
+                        tab = chrome.tabs.create({ active: false });
+                    resolve(request);
+                }
+                chrome.runtime.onMessage.addListener(listener);
+            });
+            await block;
+        });
 
-	/*
-		await chrome.scripting.executeScript({ target: { tabId: tab.id }, func: () => {
-			const button = document.getElementById("completionButton");
-			if (button) {
-		    	button.click();
-			}
-			chrome.
-		}});
-		*/
-		// await chrome.tabs.remove(tab.id);
-	}
-	chrome.runtime.onMessage.addListener(openAndMark);
+    /*
+        await chrome.scripting.executeScript({ target: { tabId: tab.id }, func: () => {
+            const button = document.getElementById("completionButton");
+            if (button) {
+                button.click();
+            }
+            chrome.
+        }});
+        */
+        // await chrome.tabs.remove(tab.id);
+    }
+    chrome.runtime.onMessage.addListener(openAndMark);
 }
 
 chrome.runtime.onMessage.addListener(openAndMark);
