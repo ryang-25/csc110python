e158dfafd1255dae070e4fab2e4113ef1040f459
Roland Yang <ryang25@governors.school>
2024-03-18 15:56:37 -0400

    up

--- isbn/isbn.py
+++ isbn/isbn.py
@@ -1,37 +1,51 @@
 # Project title: Book Code (ISBN) isbn.py
 # Author: Roland Yang
-# N/N/24
+# 3/18/24
 # On my honor I have neither given nor received unauthorized aid on this
 # assignment
 
-def checksum(isbn):
+def weighted_sum(isbn):
   isbn = isbn[:-2] # remove checksum
   nums = [int(n) for n in isbn if n != "-"] # remove dashes and work with ints
   weighted = 0
   for i in range(len(nums)):
     weighted += nums[i]*(i+1) # add 1 to account for zero based indexing
-  weighted %= 11 # take the remainder
+  return weighted
+
+def checksum(isbn):
+  weighted = weighted_sum(isbn) % 11 # take the remainder
   # remainder will never be above 10
   return "X" if weighted == 10 else str(weighted)
 
-def verify_checksum(checksum, isbn):
+def checkdigit(checksum, isbn):
   return isbn[-1] == checksum
 
 def main():
   # input
   isbn = input("Enter an ISBN number: ")
 
-  # compute
-  check_sum = checksum(isbn)
-  valid = verify_checksum(check_sum, isbn)
+  # process
+  weighted = weighted_sum(isbn)
+  check_digit = checksum(isbn)
+  isbn_check_digit = isbn[-1]
+
+  valid = checkdigit(check_digit, isbn)
 
   outcome = "Values do not match, ISBN entered is invalid"
   if valid:
     outcome = "Values match, ISBN entered is valid"
 
-  # tentative output, will probably be changed
+  # output
   print(isbn) # satisfy submitty
 
-  print("OUTCOME:", outcome)
+  print("RESULT:\n")
+  print(f"  Check sum: {weighted}\n")
+  print(f"  Check digit: {check_digit}\n")
+  print(f"  ISBN check digit: {isbn_check_digit}\n")
+
+  print(f"OUTCOME: {outcome}\n")
+  print("END RUN.")
+
+  # terminate
 
 main()
